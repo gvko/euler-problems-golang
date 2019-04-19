d0ae4f45465732cc172e835563ecc7348c893a0e
Galin Kostov <gako@esmiley.dk>
2019-04-19 14:08:29 +0200

    Problems 1 to 3 and ignore .idea/ IDE folder

---
+++ problem-1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+var bigNumber = 1000
+
+func main() {
+	sum := 0
+
+	for i := 0; i < bigNumber; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+
+	fmt.Print(sum)
+	fmt.Print("\n")
+}
---
+++ problem-2.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	end := 4000000
+	termA := 1
+	termB := 2
+	nextTerm := 0
+	sum := termB
+
+	for {
+
+		nextTerm = termA + termB
+		termA = termB
+		termB = nextTerm
+
+		if nextTerm%2 == 0 {
+			sum += nextTerm
+		}
+
+		fmt.Print(nextTerm, "\n")
+
+		if termA >= end || termB >= end {
+			break
+		}
+	}
+
+	fmt.Print("sum: ", sum, "\n")
+}
